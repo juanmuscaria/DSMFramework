f578ce260f3bebab13d964e9da086ad1e8c370ca
juanmuscaria <juanmuscaria@gmail.com>
2021-01-29 17:58:23 -0300

    Commented ExampleMod1

--- ExampleMod1/MyExampleMod1.cs
+++ ExampleMod1/MyExampleMod1.cs
@@ -2,10 +2,13 @@
 
 namespace ExampleMod1
 {
+    // This will be the entry point of the mod, you must extend IMod
     public class MyExampleMod1 : IMod
     {
+        //This method is called when your mod is being loaded
         public void Load()
         {
+            //Add a new command to the game
             CommandManager.Manager.AddCommand(new ExampleCommand());
         }
     }
@@ -16,6 +19,9 @@ namespace ExampleMod1
         {
         }
 
+        //This method is called when your command is executed
+        //Return true if the command was handled.
+        //Returning false will continue the search for any other command matching the same name as yours
         public override bool Execute(ExecutedCommand command, bool partOfMultiCommand)
         {
             ConsoleWindow3.SendConsoleResponse("An example command from ExampleMod1", ConsoleMessageType.SpecialInfo);
